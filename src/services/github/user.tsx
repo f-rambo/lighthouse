806f8574eed4cc496bd09141b035134047762ee0
frambo <13120050501@163.com>
2024-05-17 14:59:43 +0800

    sign in

--- src/services/github/user.tsx
+++
@@ -1,5 +0,0 @@
-// curl -L \
-//   -H "Accept: application/vnd.github+json" \
-//   -H "Authorization: Bearer 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" \
-//   -H "X-GitHub-Api-Version: 2022-11-28" \
-//   https://api.github.com/user
